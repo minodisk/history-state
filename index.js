7312ad6e93622fe6075ab54a6faefd107e74715d
Daisuke Mino <daisuke.mino@gmail.com>
2017-10-25 15:00:30 +0900

    Stop when error is occurred

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const log = document.querySelector('#log');
+log.style = {
+  whiteSpace: 'pre',
+};
 
 const state = {};
 let i = 0;
 let length = 0;
-setInterval(() => {
+const id = setInterval(() => {
   const value = 'abcdefghij'.repeat(100);
   state[i] = value;
   length += value.length;
@@ -11,9 +14,17 @@ setInterval(() => {
   const path = '/history-state/' + length;
   try {
     history.pushState(state, title, path);
+    log.innerText = 'pushState: ' + length + ' characters';
   } catch (err) {
+    clearInterval(id);
+    log.innreText =
+      'error ' +
+      err.name +
+      ' is occurred while pushState ' +
+      length +
+      ' characters\n' +
+      'see console for more detail';
     console.log(err);
   }
-  log.innerText = 'pushState: ' + length + 'characters';
   i++;
 }, 1000);
